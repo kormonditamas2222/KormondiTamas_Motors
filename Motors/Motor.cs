2c53e9a4cf5ddc4a3f23fa3f636b43f4422e6f73
kormonditamas2222 <t.kormond2006@gmail.com>
2025-03-14 10:14:38 +0100

    motor class

---
+++ Motors/Motor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Motors
+{
+    internal class Motor
+    {
+        private string brand;
+        private string name;
+        private int releaseYear;
+        private double performance;
+        private double priceInEur;
+
+        public Motor(string brand, string name, int releaseYear, double performance, double priceInEur)
+        {
+            this.brand = brand;
+            this.name = name;
+            this.releaseYear = releaseYear;
+            this.performance = performance;
+            this.priceInEur = priceInEur;
+        }
+
+        public string Brand { get => brand; }
+        public string Name { get => name; }
+        public int ReleaseYear { get => releaseYear; }
+        public double Performance { get => performance; }
+        public double PriceInEur { get => priceInEur; }
+    }
+}
